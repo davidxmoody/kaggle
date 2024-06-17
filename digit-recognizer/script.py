42d7af39e92f0b7d69eeb55c51b8586c19cfd4ef
David Moody <david@davidxmoody.com>
2024-06-17 15:33:43 +0100

    Display multiple random digits in a grid

--- digit-recognizer/script.py
+++ digit-recognizer/script.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import plotly.express as px
+import plotly.subplots as sp
 
 
 # %%
@@ -7,12 +8,23 @@ train = pd.read_csv("data/train.csv")
 
 
 # %%
-# Display an image of one random digit from train dataset
-random_digit = train.sample(1).values[0]
-label = random_digit[0]
-pixels = random_digit[1:].reshape((28, 28))
+num_rows = 3
+num_cols = 6
 
-fig = px.imshow(
-    pixels, color_continuous_scale="gray_r", title=f"Digit with label '{label}'"
+sample = train.sample(num_rows * num_cols).values
+
+fig = sp.make_subplots(
+    rows=num_rows, cols=num_cols, subplot_titles=[f'"{r[0]}"' for r in sample]
 )
+
+for index, item in enumerate(sample):
+    col = index % num_cols + 1
+    row = index // num_cols + 1
+    pixels = sample[index][1:].reshape((28, 28))
+    fig.add_trace(px.imshow(pixels).data[0], row=row, col=col)
+
+fig.update_xaxes(scaleanchor="y", scaleratio=1)
+fig.update_yaxes(scaleanchor="x", scaleratio=1, autorange="reversed")
+fig.update_layout(coloraxis={"colorscale": "gray_r"})
+
 fig.show()
